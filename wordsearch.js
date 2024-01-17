5e8faca26e2b440f45ec31d39ffe85510359c1ba
GardRyan <res.gardner@gmail.com>
2024-01-17 16:47:55 -0500

    Completed with JOnas

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  let newArr = [];
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      console.log(`Found word horizontally! Word is: ${word}`);
+      return true;
+    }
+  }
+
+  for (let row = 0; row < letters.length; row++) { //loop through rows
+    for (let column = 0; column < letters[row].length; column++) { //loop through columns
+      if (newArr[column] === undefined) {
+        newArr[column] = []; // if no value, skip
+      }
+      newArr[column][row] = letters[row][column]; // for position of value(index) in column & row, swap to row & column
+    }
+  }
+  //return newArr;
+  const verticalJoin = newArr.map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      console.log(`Found word vertically! Word is: ${word}`);
+      return true;
+    }
+  }
+
+  return false;
+
+};
+
+module.exports = wordSearch;
+
+//test case 1
+// word is vertical not horizontal 
+// word is horiztonal not vertical 
+// array is empty 
+
+
+//return true if the word is found, and return false if the word is not found
